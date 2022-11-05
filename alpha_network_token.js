f617edea7d6cfe407c471a9b201c5ba640826fa7
a5135324 <a5135324@gmail.com>
2022-11-05 15:31:00 +0800

    Update alpha_network_token.js

--- alpha_network_token.js
+++ alpha_network_token.js
@@ -1,18 +1,18 @@
 if ($request.headers['Cookie']) {
-	const cookie = $request.headers['Cookie'];
-	const url = $request.url;
+    const cookie = $request.headers['Cookie'];
+    const url = $request.url;
 
-	console.log('url: ' + url);
-	console.log('cookie: ' + cookie);
+    console.log('url: ' + url);
+    console.log('cookie: ' + cookie);
 
-	const saveCookie = $persistentStore.write(cookie, "AlphaNetworkCookie");
+    const saveCookie = $persistentStore.write(cookie, "AlphaNetworkCookie");
 
-	if (!(saveCookie)) {
-        $notification.post("Alpha Network Cookie 保存錯誤‼️", "", "請重新登入")
+    if (!(saveCookie)) {
+        $notification.post("α Network Cookie 保存錯誤‼️", "", "請重新登入")
     } else {
-        $notification.post("Alpha Network Cookie 保存成功🎉", "", "")
+        $notification.post("α Network Cookie 保存成功🎉", "", "")
     }
 } else {
-    $notification.post("Alpha Network Cookie 保存失敗‼️", "", "請重新登入")
+    $notification.post("α Network Cookie 未發現‼️", "", "請重新開啟 app")
 }
 $done({})
