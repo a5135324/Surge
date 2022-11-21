31098ffc1b643333be532c60e7643b4be0d6c1aa
a5135324 <a5135324@gmail.com>
2022-11-21 15:38:47 +0800

    Update alpha_network_token.js

--- alpha_network_token.js
+++ alpha_network_token.js
@@ -12,6 +12,20 @@ if ($request.headers['Cookie']) {
     } else {
         $notification.post("α Network Cookie 保存成功🎉", "", "")
     }
+} else if ($request.headers['cookie']) {
+    const cookie = $request.headers['cookie'];
+    const url = $request.url;
+
+    console.log('url: ' + url);
+    console.log('cookie: ' + cookie);
+
+    const saveCookie = $persistentStore.write(cookie, "AlphaNetworkCookie");
+
+    if (!(saveCookie)) {
+        $notification.post("α Network Cookie 保存錯誤‼️", "", "請重新登入")
+    } else {
+        $notification.post("α Network Cookie 保存成功🎉", "", "")
+    }
 } else {
     $notification.post("α Network Cookie 未發現‼️", "", "請重新開啟 app")
 }
